17c278f33e0aa25720a7abc4065641a29e846461
Shane Snover <ssnover95@gmail.com>
2021-01-23 12:36:41 -0500

    Parsed the structure to get just topic names for publications and subscriptions, it probably repeats duplicates right now...

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use xmlrpc::{Request, Value};
+use xmlrpc::Request;
 
 fn main() {
 
@@ -6,5 +6,27 @@ fn main() {
 
     let result = topics_request.call_url("http://192.168.1.22:11311");
 
-    println!("Topics: {:#?}", result.unwrap()[2]);
+    let system_state = result.unwrap();
+
+    let mut topics_list: Vec<String> = vec![];
+    let topic_types = &system_state[2];
+    let publishers = &topic_types[0];
+    for topic in publishers.as_array().unwrap() {
+        match topic[0].as_str() {
+            Some(topic_name) => topics_list.push(topic_name.to_string()),
+            None => (),
+        }
+    }
+
+    let subscribers = &topic_types[1];
+    for topic in subscribers.as_array().unwrap() {
+        match topic[0].as_str() {
+            Some(topic_name) => topics_list.push(topic_name.to_string()),
+            None => (),
+        }
+    }
+
+    for topic in topics_list {
+        println!("{}", topic);
+    }
 }
